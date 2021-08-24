ebcd3394fd85de8bde258c2f64dd03ce52df51fa
Jagadeesh C <jagac553@gmail.com>
2021-08-24 08:54:19 +0530

    Initial Commit

---
+++ pythonFile.py
@@ -0,0 +1,15 @@
+def display():
+    print('display function called')
+
+    for i in range(10):
+        print(i + 1)
+
+    print('display function ended')
+
+
+
+print('Hello, Welcome!')
+
+print('this is a python file')
+
+display()
